2bb575cf7e5ab63dcd36d7227e19878756573896
Daniel Atanasov <daniel.a.atanasov97@gmail.com>
2022-02-05 19:24:19 +0200

    fix: Fix theme playing and re-add "set my theme" with queries

--- assnouncer.py
+++ assnouncer.py
@@ -2,23 +2,14 @@ from __future__ import annotations
 
 import util
 
+from util import LoadedSong
 from dataclasses import dataclass, field
 from asyncio.tasks import sleep
-from typing import Any, List
+from typing import List
 from pathlib import Path
 from discord.player import AudioPlayer
 from discord import Client, Game, FFmpegOpusAudio, TextChannel, Message, Guild, VoiceClient, Member, VoiceState
-
-from pytube import YouTube, Search
-
 from commands import BaseCommand
-from downloaders import BaseDownloader
-
-
-@dataclass
-class LoadedSong:
-    uri: str
-    source: FFmpegOpusAudio
 
 
 @dataclass
@@ -31,39 +22,31 @@ class Assnouncer(Client):
     def __post_init__(self):
         super().__init__()
 
-    @staticmethod
-    def search_song(query: str) -> str:
-        results: List[YouTube]
-        results, _ = Search(query).fetch_and_parse()
-        if results:
-            return results[0].watch_url
-
     def is_playing(self) -> bool:
         return self.voice.is_playing()
 
     def skip(self):
-        self.voice.stop
+        self.voice.stop()
 
     def stop(self):
         self.queue = []
         self.skip()
 
     async def play_now(self, source: FFmpegOpusAudio):
-        vc: VoiceClient = self.voice
-        if vc.is_playing():
-            old_source: AudioPlayer = gg.voice._player
+        if self.voice.is_playing():
+            old_source: AudioPlayer = self.voice._player
             old_source.pause()
 
-            vc._player = None
-            vc.play(source)
-            while vc.is_playing():
+            self.voice._player = None
+            self.voice.play(source)
+            while self.voice.is_playing():
                 await sleep(0.3)
-            vc.stop()
+            self.voice.stop()
 
-            vc._player = old_source
-            vc.resume()
+            self.voice._player = old_source
+            self.voice.resume()
         else:
-            vc.play(source)
+            self.voice.play(source)
 
     def set_activity(self, activity: str):
         return self.change_presence(activity=Game(name=activity))
@@ -74,35 +57,10 @@ class Assnouncer(Client):
 
         return channel.send(message)
 
-    async def download_song(self, uri: str) -> FFmpegOpusAudio:
-        downloaders = BaseDownloader.get_instances()
-        if all(not downloader.accept(uri) for downloader in downloaders):
-            uri = Assnouncer.search_song(uri)
-
-        filename = util.get_download_path(uri)
-
-        async def load_song():
-
-            return LoadedSong(
-                uri=uri,
-                source=await util.load_source(filename)
-            )
-
-        if filename.is_file():
-            return await load_song()
-
-        for downloader in downloaders:
-            if downloader.accept(uri):
-                print(f"[info] Downloading via {downloader.__name__}")
-                if downloader.download(uri, filename):
-                    break
-
-        return await load_song()
-
     async def download_from_queue(self, idx: int) -> LoadedSong:
         if len(self.queue) > idx:
             uri = self.queue[idx]
-            return await self.download_song(uri)
+            return await util.download(uri)
 
     async def song_loop(self):
         def pop(*_):
@@ -112,13 +70,13 @@ class Assnouncer(Client):
         while True:
             while self.is_playing():
                 if len(self.queue) > 1:
-                    await self.download_from_queue(1)
+                    await util.download(self.queue[1])
                 await sleep(0.1)
 
             while not self.queue:
                 await sleep(0.3)
 
-            song = await self.download_from_queue(0)
+            song = await util.download(self.queue[0])
 
             await self.message(f"Playing '{song.uri}'")
 
@@ -144,7 +102,7 @@ class Assnouncer(Client):
     def queue_song(self, query: str):
         self.queue.append(query)
 
-    async def play_theme(self, user: str):
+    async def play_theme(self, user: Member):
         theme_path = util.get_theme_path(user)
         new_source = util.load_source(theme_path)
 
@@ -165,15 +123,15 @@ class Assnouncer(Client):
         prev_channel = before.channel
         next_channel = after.channel
         if prev_channel is None and next_channel is not None:
-            print(f"[chat] <{next_channel.name}: {member} has joined")
-            await self.play_theme(f"{member.name}#{member.discriminator}")
+            print(f"[chat] <{next_channel.name}>: {member} has joined")
+            await self.play_theme(member)
 
     async def on_message(self, message: Message):
         if self.voice is None:
             return
 
         for command in BaseCommand.get_instances():
-            command_args = command.parse(message.content)
+            command_args = command.parse(message)
             if command_args is not None:
                 print(f"[info] Received {command.__name__}")
                 await command.on_command(self, command_args)
